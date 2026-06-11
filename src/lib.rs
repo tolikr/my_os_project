995dc60535bc0506d82f1e6e100cdc9654d624e8
Anatoliy Botov <1284004+tolikr@users.noreply.github.com>
2026-06-11 23:28:15 +0300

    Parse fdt

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,8 @@
 
 extern crate alloc; // Подключаем стандартный системный крейт кучи Rust
 
-mod uart;
 mod memory;
+mod uart;
 
 use core::panic::PanicInfo;
 use uart::Uart;
@@ -14,11 +14,10 @@ static ALLOCATOR: memory::heap::LinkedListAllocator = memory::heap::LinkedListAl
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kmain(hart_id: usize, fdt_address: usize) {
-
     Uart::print_ln("Kernel loaded.");
 
     memory::init();
-    
+
     Uart::print_ln("Memory subsystem fully initialized (Pages + Heap).");
 
     // ПРОВЕРКА: Теперь у вас работает магия Rust!
@@ -26,11 +25,10 @@ pub extern "C" fn kmain(hart_id: usize, fdt_address: usize) {
     let mut v = Vec::new();
     v.push(hart_id);
     v.push(fdt_address);
-    
+
     Uart::print_ln("Vec successfully allocated on RISC-V 64 heap!");
 
     Uart::print_str("--------------------------\n");
-
 
     Uart::print_ln("Hart id.");
     Uart::print_hex(hart_id);
@@ -39,8 +37,42 @@ pub extern "C" fn kmain(hart_id: usize, fdt_address: usize) {
     Uart::print_hex(fdt_address);
     Uart::print_ln("");
 
-    
-    Uart::print_str("--------------------------\n");
+    // 2. Инициализируем парсер FDT по указателю, который нам дал QEMU
+    unsafe {
+        match fdt::Fdt::from_ptr(fdt_address as *const u8) {
+            Ok(fdt) => {
+                Uart::print_ln("FDT successfully parsed!");
+
+                // Читаем модель платы из FDT
+                Uart::print_str("Booted on board: ");
+                Uart::print_ln(fdt.root().model());
+
+                // Узнаем, сколько памяти нам РЕАЛЬНО выделил QEMU
+                let memory = fdt.memory();
+
+                Uart::print_ln("Detected RAM regions:");
+                for region in memory.regions() {
+                    Uart::print_str("  - Start: ");
+                    Uart::print_hex(region.starting_address as usize);
+                    Uart::print_str("    Size: ");
+                    Uart::print_hex(region.size.unwrap_or(0) as usize);
+                }
+
+                // Посчитаем количество ядер процессора (Harts)
+                let cpus_count = fdt.cpus().count();
+                Uart::print_ln("");
+                Uart::print_ln("Number of CPU cores: ");
+                // (Для вывода чисел можно использовать Uart::print_hex или перевести в строку)
+                Uart::print_hex(cpus_count);
+            }
+            Err(_) => {
+                Uart::print_ln("ERROR: Failed to parse FDT header!");
+            }
+        }
+    }
+
+    Uart::print_ln("");
+    Uart::print_ln("--------------------------");
 }
 
 #[panic_handler]
